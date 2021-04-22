6809c595a86cb63f3289ed5ae954757aa479e68f
Yisong Zou <yiosng.zou@duke.edu>
2021-04-22 16:58:33 +0000

    Finished

--- app.py
+++ app.py
@@ -22,17 +22,5 @@ def predict():
 
     return render_template('index.html', prediction_text='Salary is {}'.format(output))
 
-
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    '''
-    For direct API calls trought request
-    '''
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
-
-    output = prediction[0]
-    return jsonify(output)
-
 if __name__ == "__main__":
     app.run(debug=True)
