103f3f5f0c78cd5080dae6cac437d8ef5cc0e2b6
Yisong Zou <yiosng.zou@duke.edu>
2021-07-30 00:39:50 +0000

    Fixed some parts

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def predict():
     #output = round(prediction[0], 2)
     output1 = round(prediction[0], 0)
     output = int(output1)
-    return render_template('index.html', prediction_text='Medal Number is {}'.format(output))
+    return render_template('index.html', prediction_text='Gold Medal Number is {}'.format(output))
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8080, debug=True)
